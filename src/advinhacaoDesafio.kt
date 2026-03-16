7571d27ed54b5a5017d85fcf09a6e52815e9bde2
yurigrade034 <gradeyuri1@gmail.com>
2026-03-16 19:24:53 -0300

    refatoraçao de mensagens

--- src/advinhacaoDesafio.kt
+++ src/advinhacaoDesafio.kt
@@ -6,42 +6,39 @@ fun main() {
 }
 
 fun jogoAdivinhacao() {
-
-    val cartasParaEmbaralhar = mutableListOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+    val cartasParaEmbaralhar = mutableListOf<Any>(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14)
     cartasParaEmbaralhar.shuffle()
 
-    val pilhaDeCartas = ArrayDeque<Int>()
+    val pilhaDeCartas = ArrayDeque<Any>()
     for (carta in cartasParaEmbaralhar) {
         pilhaDeCartas.addLast(carta)
     }
 
-    thread(name = "Log ADM") {
+    thread(name = "Log ADM", isDaemon = true) { //processo de segundo plano
         while (pilhaDeCartas.isNotEmpty()) {
-            val topoAtual = pilhaDeCartas.lastOrNull()
+            val topoAtual = try { pilhaDeCartas.lastOrNull() } catch (e: Exception) { null }
             if (topoAtual != null) {
-
                 println("\n[${Thread.currentThread().name}]: O topo atual é $topoAtual")
             }
             Thread.sleep(5000)
-        }
+        } //tenta ler o topo e se nao der nada apenas retorna nulo
     }
 
     var pontos = 0
-    println("---> Jogo de Adivinhação (Pilhas) <---")
-    println("Tente adivinhar a carta do topo, tem ${pilhaDeCartas.size} cartas.")
+    println("---> Jogo de Adivinhação Pilhas <---")
+    println("acerte a carta do topo, tem ${pilhaDeCartas.size} cartas")
 
     while (pilhaDeCartas.isNotEmpty()) {
-
         val cartaDoTopo = pilhaDeCartas.last()
 
-        print("\nQual número 1 a 10 está no topo da pilha? ")
+        print("\nqual número 1 a 14 está no topo da pilha? ")
         val palpite = readLine()?.toIntOrNull()
 
         if (palpite == cartaDoTopo) {
-            println("Acertou baguá, carta era $cartaDoTopo.")
+            println("acertou baguá, carta era $cartaDoTopo.")
             pontos += 10
         } else {
-            println("Errou baguá, carta era $cartaDoTopo.")
+            println("errou baguá, carta era $cartaDoTopo.")
         }
 
         pilhaDeCartas.removeLast()
@@ -50,5 +47,5 @@ fun jogoAdivinhacao() {
     }
 
     println("\n---> FIM DE JOGO <---")
-    println("Sua pontuação final: $pontos pontos")
+    println("total de $pontos pontos")
 }
